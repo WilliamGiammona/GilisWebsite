7156e9aad9564cf6e4ff69cbf09ec91dc3c2d8de
William Giammona <wgiammona@gmail.com>
2025-01-14 18:45:45 +0200

    increased horizontal padding

--- app/(work)/illustrations/page.tsx
+++ app/(work)/illustrations/page.tsx
@@ -54,7 +54,7 @@ const IllustrationsPage = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-[rgb(24,24,24)] text-white p-8">
+    <div className="min-h-screen bg-[rgb(24,24,24)] text-white p-8 px-12 ">
       <h1 className="text-3xl font-light mb-12">Illustrations</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
